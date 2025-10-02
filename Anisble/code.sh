bdc0bab31758c792adf7b6fcce8bbf6f74923ebd
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-02 12:12:29 +0530

    1

--- Anisble/code.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-sudo dnf install -y python3 python3-venv python3-devel epel-release
-python3 -m venv ~/ansible-venv
-source ~/ansible-venv/bin/activate
-pip install --upgrade pip
-pip install "ansible==2.16.*"
-ansible --version
-echo "Run 'source ~/ansible-venv/bin/activate' to activate Ansible env later"
