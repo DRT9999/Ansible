417daa568622d088093f1fb26228c4c81a5499d9
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-06 02:27:12 +0530

    GoCD

--- GOCD_Install/install.sh
+++ GOCD_Install/install.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+# #!/bin/bash
 
-stat() {
-    if [ $1 -eq  0 ] ; then 
-        echo -e "\e[32m - Success \e[0m" 
-    else 
-        echo -e "\e[31m - Failure \e[0m" 
-        exit 1
-    fi 
-}
+# stat() {
+#     if [ $1 -eq  0 ] ; then 
+#         echo -e "\e[32m - Success \e[0m" 
+#     else 
+#         echo -e "\e[31m - Failure \e[0m" 
+#         exit 1
+#     fi 
+# }
 
-if awk -F: '{print $1}' /etc/passwd | grep -q "^go$"; then
-    echo "User 'go' exists"
-else
-    useradd -m -s /bin/bash go
-fi
-stat $?
+# if awk -F: '{print $1}' /etc/passwd | grep -q "^go$"; then
+#     echo "User 'go' exists"
+# else
+#     useradd -m -s /bin/bash go
+# fi
+# stat $?
 
-java -version
+# java -version
 
-dnf install java-17-openjdk
-stat $?
+# dnf install java-17-openjdk
+# stat $?
 
-java --version
-stat $?
+# java --version
+# stat $?
 
-cd /home/go
+# cd /home/go
 
-wget https://download.gocd.org/binaries/24.3.0-19261/generic/go-server-24.3.0-19261.zip
-stat $?
+# wget https://download.gocd.org/binaries/24.3.0-19261/generic/go-server-24.3.0-19261.zip
+# stat $?
 
-
+# unzip 
